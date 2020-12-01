32ff8d2bf8128a9a398ef6148880748076f5903f
falak08 <73591794+falak08@users.noreply.github.com>
2020-12-01 23:49:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+var tree1;
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+tree1=new tree(200,200,20,20);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  tree1.display();
+  
+ 
+}
+
+
+
